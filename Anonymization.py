b5a7f916b9f7e081e76ea226f669e6911bf2a584
Magnus <56515134+Magnsta@users.noreply.github.com>
2023-06-07 22:53:53 +0200

    Update Anonymization.py

--- Anonymization.py
+++ Anonymization.py
@@ -1,8 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO("best.pt") #Load model
 
+model = YOLO("weight.pt") #Load model
 
-#Set anonymization TRUE to enable the anonymization filter. 
-#If set to FALSE, standard YOLOv8 code is running.
+#Set anonymization=True to enable the anonymization filter. 
 model.predict(source=0, show=True,imgsz=640,save=True,anonymization=True)
